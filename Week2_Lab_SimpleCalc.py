27f2caed051c244bcedff55f8558986230fc2f1c
Ifo0 <ivo.petrov94@gmail.com>
2018-10-15 00:03:02 +0100

    Trying to Commit lecture notes

---
+++ Week2_Lab_SimpleCalc.py
@@ -0,0 +1,76 @@
+# P = a + b + a + b
+# S = a.b
+
+#Zadacha 1
+# x1 = float(input())
+# y1 = float(input())
+# x2 = float(input())
+# y2 = float(input())
+#
+#
+# strana_a = abs(x1 - x2)
+# strana_b = abs(y2 - y1)
+#
+# lice = strana_a * strana_b
+# perimetur = 2 * (strana_a + strana_b)
+#
+# print(lice)
+# print(perimetur)
+
+
+# #Zadacha 2
+#
+# # 1 USD = 1.79549 BGN.
+#
+# dollars = float(input())
+# lev = dollars * 1.79549
+#
+# print(f"{lev:.2f}")
+# import math
+# print(f'The value of pi is approximately {math.pi:.3f}.')
+
+#Zadacha 3
+
+
+# stoinost = float(input())
+# vhodna_valuta = input()
+# izhodna_valuta = input()
+#
+# kursove = {"USD": 1.79549,"EUR": 1.95583 , "GBP": 2.53405, "BGN": 1}
+#
+# convertirana_valuta = stoinost * (kursove[vhodna_valuta] / kursove[izhodna_valuta])
+#
+# print(f"{convertirana_valuta:.2f}")
+
+
+#Zadacha 4
+
+# broi_masi = int(input())
+# dulzina_masi = float(input())
+# shirochina_masi = float(input())
+#
+# cena_kvadrat_pokrivka = 7
+# cena_kvadrat_kare = 9
+# kurs_bgn = 1.85
+#
+# plosht_pokrivki = broi_masi * ((dulzina_masi + 2 * 0.30) * (shirochina_masi + 2 * 0.30))
+# plosht_kareta = broi_masi * ((dulzina_masi / 2) * (dulzina_masi / 2))
+#
+# cena_pokrivki = plosht_pokrivki * cena_kvadrat_pokrivka
+# cena_kareta = plosht_kareta * cena_kvadrat_kare
+# obshta_cena = cena_pokrivki + cena_kareta
+# print(f'{obshta_cena:.2f} USD')
+# print(f'{obshta_cena * kurs_bgn:.2f} BGN')
+
+
+
+
+
+
+
+
+
+
+
+
+
